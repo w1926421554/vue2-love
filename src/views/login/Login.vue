7fd121e963d2c3fa846ca132818aaf16746d6279
unknown <1358213862@qq.com>
2023-01-09 18:03:08 +0800

    login

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        登录
+    <div class="root">
+        <div class="header">
+            <div></div>
+            <div>登录 -- 恋爱吧</div>
+        </div>
+        <div class="nav"></div>
+        <div class="container"></div>
     </div>
 </template>
 
@@ -11,5 +16,20 @@
 </script>
 
 <style lang="scss" scoped>
-
+.root{
+    display: flex;
+    flex-direction: column;
+    .header{
+        flex: 0 0 100px;
+        background-color: #393a3f;
+    }
+    .nav{
+        flex: 0 0 150px;
+        background-color: #0084ff;
+    }
+    .container{ 
+        flex:1;
+        background-color: #000;
+    }
+}
 </style>
