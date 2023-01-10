6d5ac6e0a54923c6087d3117da110d7ece46e82f
1926421554@qq.com <1926421554@qq.com>
2023-01-10 11:54:55 +0800

    home

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button,Form,Field  } from 'vant';
+
+import { Button,Form,Field,Swipe, SwipeItem,Grid, GridItem } from 'vant';
 Vue.config.productionTip = false
 
-Vue.use(Button).use(Form).use(Field);
+Vue.use(Button).use(Form).use(Field).use(Swipe).use(SwipeItem).use(Grid).use(GridItem);
 new Vue({
   router,
   store,
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        主页
+  <div>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+    <div class="box-color"></div>
+    <div class="main">
+        <!--  -->
+      <van-grid :column-num="5">
+        <van-grid-item icon="photo-o" text="高富帅" />
+        <van-grid-item icon="photo-o" text="白富美" />
+        <van-grid-item icon="photo-o" text="才俊" />
+        <van-grid-item icon="photo-o" text="动态" />
+        <van-grid-item icon="photo-o" text="编辑资料" />
+      </van-grid>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+.box-color {
+  height: 50px;
+  background-color: #8051d6;
+}
+.main{
+    // position: absolute;
+    padding: 0 15px;
+}
 </style>
