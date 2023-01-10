ffe515fbe111d890833d7593ba1105490f9ade22
1926421554@qq.com <1926421554@qq.com>
2023-01-10 16:06:49 +0800

    proxy

--- src/utlis/request.js
+++ src/utlis/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // 配置服务器地址
 const SERVER_AX = axios.create({
-    baseURL: "http://38.60.28.154:9999/", //服务器基本路径
+    baseURL: "/api", //服务器基本路径
     timeout: 8000, //超时
   });
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 服务配置
+  devServer:{
+    // port: 8080, // 启用8080端口
+    open: false,  // 启动后自动打开浏览器
+    proxy:{
+      '/api':{
+        target: 'http://38.60.28.154:9999/', // 换成后端地址
+        // ws:true,
+        changeOrigin: true,
+        pathRewrite:(path) => path.replace(/^\/api/, "")
+      }
+    }
+  }
 })
